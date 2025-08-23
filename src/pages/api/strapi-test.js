b59410bf4abe3677ef0dabee2f5d5e9d7149a162
Loc <locqdang@gmail.com>
2025-08-23 00:18:21 -0400

    going to bed

--- src/pages/api/strapi-test.js
+++
@@ -1,7 +0,0 @@
-import { fetchHomepage } from "../../../lib/homepage";
-import { fetchNavbar } from "../../../lib/navbar";
-
-export default async function handler(req, res) {
-  const data = await fetchHomepage();
-  return res.status(200).json(data);
-}
