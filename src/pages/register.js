82a15907a5061687e123eb784f9c3483a1be605b
loc <locqdang@gmail.com>
2026-05-18 11:21:06 -0400

    removed login, register and passport

--- src/pages/register.js
+++
@@ -1,9 +0,0 @@
-import RegisterForm from '../components/RegisterForm';
-
-export default function Register() {
-  return (
-    <div className="flex min-h-screen items-start mt-6 justify-center">
-      <RegisterForm />
-    </div>
-  );
-}
